7516281c685e59ace231d42a171c55ea43664db4
Noah May <noahmouse2011@gmail.com>
2024-08-30 16:09:53 -0500

    Basic window management

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
 import pygame
 
-def main():
-    print("Starting asteroids!")
+from constants import *
 
-if __name__ == '__main__':
+
+def main():
+    pygame.init()
+
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    print("Starting asteroids!")
+    print("Screen width:", SCREEN_WIDTH)
+    print("Screen height:", SCREEN_HEIGHT)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill((0, 0, 0))
+        pygame.display.flip()
+
+
+if __name__ == "__main__":
     main()
