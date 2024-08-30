9fae909d7d4e1d1b3233ada6e4052dfb5c54a624
Noah May <noahmouse2011@gmail.com>
2024-08-30 16:20:25 -0500

    Add clock to stabalize framerate

--- main.py
+++ main.py
@@ -11,12 +11,17 @@ def main():
     print("Screen width:", SCREEN_WIDTH)
     print("Screen height:", SCREEN_HEIGHT)
 
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0))
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
