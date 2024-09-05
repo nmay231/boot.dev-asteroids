2a0bab33f482b09a5a0e46010b84f7a13dea0c8f
Noah May <noahmouse2011@gmail.com>
2024-09-05 14:45:19 -0500

    Draw the player as a triangle

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 
-from constants import *
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+from player import Player
 
 
 def main():
@@ -14,11 +15,16 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
         screen.fill((0, 0, 0))
+        player.draw(screen)
+
         pygame.display.flip()
 
         dt = clock.tick(60) / 1000
